8e220562419219642f4a51a7982dd1bfac15936c
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-23 21:51:13 +0800

    Add auto-backup config files

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -2,5 +2,6 @@
 
 VERSION="$(curl --retry 10 --retry-max-time 60 https://api.github.com/repos/iawia002/lux/releases/latest | jq .tag_name | sed 's/\"//g;s/v//g')"
 curl -s --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/iawia002/lux/releases/download/v${VERSION}/lux_${VERSION}_Linux_64-bit.tar.gz -o - | tar -zxf - -C /usr/bin
+echo ${RCLONE_CONFIG_BASE64} | base64 -d  >/.aria2allinoneworkdir/drive.conf
 
 exec runsvdir -P /etc/service
