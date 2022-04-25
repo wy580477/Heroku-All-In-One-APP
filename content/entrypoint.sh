aa1da2d0790ced5fea660132733ac052d17b6fd4
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-25 19:08:57 +0800

    Update entrypoint.sh

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 -m pip install -U yt-dlp
+python3 -m pip install -U yt-dlp &
 mkdir -p /mnt/data/config /mnt/data/downloads /mnt/data/videos
 echo ${RCLONE_CONFIG_BASE64} | base64 -d  >/mnt/data/config/rclone.conf
 
