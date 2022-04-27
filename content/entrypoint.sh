e03a75e62fcda25d1199ae64f276825b7c1a29b4
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-27 08:46:46 +0800

    auto-update yt-dlp

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-python3 -m pip install -U yt-dlp &
 mkdir -p /mnt/data/config /mnt/data/downloads /mnt/data/videos
 mv /.aria2allinoneworkdir/bashrc /mnt/data/config/
 echo ${RCLONE_CONFIG_BASE64} | base64 -d  >/mnt/data/config/rclone.conf
